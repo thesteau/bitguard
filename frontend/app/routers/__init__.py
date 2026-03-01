7e5dd7de907d20cdeb55073d8a1fde4c9beb0c23
thesteau <steau415@gmail.com>
2026-03-01 13:23:33 -0800

    Reformat

--- frontend/app/routers/__init__.py
+++ frontend/app/routers/__init__.py
@@ -1,6 +1,7 @@
 from .pages import router as pages_router
 from .blog import router as blog_router
 
+
 def include_routers(app):
     app.include_router(pages_router)
     app.include_router(blog_router)
