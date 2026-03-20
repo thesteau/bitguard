2d49973dec0b2a246fbdb094a690bd9ff916f3ad
thesteau <steau415@gmail.com>
2026-03-19 22:22:01 -0700

    Add a test seed endpoint

--- backend/database/app/main.py
+++ backend/database/app/main.py
@@ -23,6 +23,15 @@ driver = GraphDatabase.driver(
 async def test_connection():
     return {"message": "Connection to database successful!"}
 
+@app.get("/test2")
+async def test_connection2():
+    parameters = {"seed_parameter": envs.TEST_SEED}
+    with driver.session(database=DATABASE) as session:
+        result = session.run(query, parameters)
+        records = [record.data() for record in result]
+
+    return {"records": records}
+
 @app.post("/query")
 async def query_database(request: Request):
     body = await request.json()
