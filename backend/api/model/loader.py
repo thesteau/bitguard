7f12c7f439638fe80479cbde41ab73e1f5483ed0
thesteau <steau415@gmail.com>
2026-03-22 18:04:07 -0700

    Rename core to app to standardize directories

--- backend/api/model/loader.py
+++ backend/api/model/loader.py
@@ -1,6 +1,6 @@
 import pickle
 
-from core.environments import environments as env
+from app.environments import environments as env
 
 with open(env.MODEL_PATH, 'rb') as f:
     lgb_model = pickle.load(f)
